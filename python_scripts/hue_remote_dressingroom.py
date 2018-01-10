262e1fa0b9f305b397e639959e7219d5ecccd5e2
Jan-Erik Dahlin <janerikdahlin@yahoo.com>
2018-01-10 23:04:50 +0100

    First working setup

---
+++ python_scripts/hue_remote_dressingroom.py
@@ -0,0 +1,10 @@
+button_obj = hass.states.get('sensor.dressingroom_switch') 
+button = button_obj.state 
+if button == '1_click': 
+  hass.services.call('script', 'turn_on', { "entity_id" : 'script.dressingroom_button_1' }) 
+elif button == '2_click': 
+  hass.services.call('script', 'turn_on', { "entity_id" : 'script.dressingroom_button_2' }) 
+elif button == '3_click': 
+  hass.services.call('script', 'turn_on', { "entity_id" : 'script.dressingroom_button_3' }) 
+elif button == '4_click': 
+  hass.services.call('script', 'turn_on', { "entity_id" : 'script.dressingroom_button_4' }) 

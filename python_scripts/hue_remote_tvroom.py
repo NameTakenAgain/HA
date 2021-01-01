bf7eaaaa856e97bd00fd5b0fb0407243f79d0244
Jan-Erik Dahlin <janerikdahlin@yahoo.com>
2021-01-01 11:08:44 +0100

    Before upgrade

--- python_scripts/hue_remote_tvroom.py
+++ python_scripts/hue_remote_tvroom.py
@@ -1,10 +1,10 @@
 button_obj = hass.states.get('sensor.tvroom_switch') 
 button = button_obj.state 
-if button == '1_click': 
+if button == '1_click_up': 
   hass.services.call('script', 'turn_on', { "entity_id" : 'script.tvroom_button_1' }) 
-elif button == '2_click': 
+elif button == '2_click_up': 
   hass.services.call('script', 'turn_on', { "entity_id" : 'script.tvroom_button_2' }) 
-elif button == '3_click': 
+elif button == '3_click_up': 
   hass.services.call('script', 'turn_on', { "entity_id" : 'script.tvroom_button_3' }) 
-elif button == '4_click': 
+elif button == '4_click_up': 
   hass.services.call('script', 'turn_on', { "entity_id" : 'script.tvroom_button_4' }) 
